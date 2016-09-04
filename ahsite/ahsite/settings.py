5d828d5adc7566090e237ea57b8aa02a2980ece6
Luiz Felipe <luizfelipe.unesp@gmail.com>
2016-09-04 20:03:10 +0100

    heroku test

--- ahsite/ahsite/settings.py
+++ ahsite/ahsite/settings.py
@@ -108,3 +108,8 @@ TINYMCE_DEFAULT_CONFIG = {
 }
 TINYMCE_SPELLCHECKER = True
 TINYMCE_COMPRESSOR = True
+
+#heroku
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)

604219e75be088b635c4ae0d732c8c5d7d286c1a
Luiz Felipe <luizfelipe.unesp@gmail.com>
2016-09-04 21:04:12 +0100

    heroku test

--- ahsite/ahsite/settings.py
+++ ahsite/ahsite/settings.py
@@ -92,12 +92,10 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-#     '/home/luizfelipe/Documents/codes/ah-website/ahsite/static/',
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-print STATIC_ROOT       
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/home/luizfelipe/Documents/codes/ah-website/ahsite/static/',
+]
 
 ##### custom 
 

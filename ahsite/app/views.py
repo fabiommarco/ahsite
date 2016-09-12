54070d2be18f07f1345739ccf34e4bad77cd86d7
Luiz Felipe <luizfelipe.unesp@gmail.com>
2016-09-12 19:21:38 +0100

    improvments

--- ahsite/app/views.py
+++ ahsite/app/views.py
@@ -35,6 +35,7 @@ def load_basic_info(method):
 def home(request):
     latest_feeds = News.objects.order_by('-news_date')[:5]
     return render(request, 'index.html', {'is_index':True,
+                                          'products':Products.objects.all(),
                                           'general_info':general_info,
                                           'events_link':events_link,
                                           'partners_link':partners_link,

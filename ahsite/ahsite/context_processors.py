c986bf2e43cd086ba1bda383429a28ee9d698270
Victor Cinaglia <victorcinaglia@gmail.com>
2017-09-04 09:47:36 -0400

    Fix error when rendering 404 pages

--- ahsite/ahsite/context_processors.py
+++ ahsite/ahsite/context_processors.py
@@ -14,10 +14,11 @@ def i18n_url(request):
     if current_language != settings.LANGUAGE_CODE:
         activate(settings.LANGUAGE_CODE)
         resolver_match = request.resolver_match
-        redirect_to = reverse(resolver_match.url_name,
-                              args=resolver_match.args,
-                              kwargs=resolver_match.kwargs)
-        activate(current_language)
+        if resolver_match:
+            redirect_to = reverse(resolver_match.url_name,
+                                  args=resolver_match.args,
+                                  kwargs=resolver_match.kwargs)
+            activate(current_language)
     return {"redirect_to": redirect_to}
 
 def load_info(request):
